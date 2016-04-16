782f1694e29750768685fe079f732b116c4cac42
cloverharvest <anna@schwabdev.com>
2016-04-16 13:12:42 -0700

    Delete function in show.html works and redirects to home page

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -14,9 +14,22 @@ function BooksShowController($http, $routeParams, $location) {
     method: 'GET',
     url: endpoint + bookId,
   }).then(function successCallback(response) {
-    console.log(response.data);
+    console.log("this is the book that was clicked: ", response.data);
     vm.book = response.data;
   }, function errorCallback(response) {
     console.log('There was an error getting the data', error);
   });
+
+  vm.deleteBook = function(book) {
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + book._id,
+    }).then(function successCallback(json) {
+      console.log("the book to be deleted is: ", book._id);
+      //this deletes the book and returns the user to the home page
+      $location.path('/');
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response.data);
+    });
+  };
 }
